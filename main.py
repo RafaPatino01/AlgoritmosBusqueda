6d231912ec69df3f902ede97636b20701d514bf7
inaki321 <90475192+inaki321@users.noreply.github.com>
2022-03-06 21:12:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from grafo import *
 
 myCSV = "/Volumes/GoogleDrive/My Drive/Universidad/Semestre 6/IA/proyecto_busqueda/Grafoproyecto.csv"
 grafo = Grafo(myCSV)
+grafo = Grafo(myCSV)
 
 # Profundidad limitada
-grafo.profundidadLim("A", "G")
+grafo.BFS('A','G')
+print('-------------------------------')
+grafo.DFS('A','G')
+print('-------------------------------')
+grafo.DFSLim('A','I')
+print('-------------------------------')
+grafo.IDFS("A", "G", 1)
+print('-------------------------------')
+
 
 # Best First
-grafo.bestFirst("A", "G")
-
-
+#grafo.bestFirst("A", "G")
