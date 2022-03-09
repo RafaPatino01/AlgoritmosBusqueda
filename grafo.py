1f28e1b053d826e8b28c014651bbee83772f053b
inaki321 <90475192+inaki321@users.noreply.github.com>
2022-03-09 15:12:04 -0600

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -29,7 +29,7 @@ class Grafo:
                     aux += j[2:]
                     nodos.append(aux)
 
-
+        self.nodosLista = nodos
         #diccionario de conexiones
         #{
             # 'S': [('A', 3), ('D', 4)],
@@ -53,6 +53,31 @@ class Grafo:
     def verticesConexion(self, v):
         return self.listaConexiones[v] #regresa la lista de conexioens de "v" nodo 
 
+    #funcion que nos devuelve la heuristica
+    def heuristica(self, n):
+        #recibe el nodo a utilizar
+        dcoord, ncoord = (0, 0), (0, 0)
+        #dcoord son las coordenadas del nodo destino
+        #ncoord son las coordenadas del nodo en curso
+        heu = None
+        
+        #aqui se sacan los valores para x1 y y1
+        #que son los del destino
+        for i in self.nodosLista:
+            if i[0] == self.nodoDestino:
+                dcoord = (i[3], i[4]) 
+                break
+            
+        
+        for i in self.nodosLista:
+            if i[0] == n:#checar los valores de x2 y y2, de mi nodo actual
+                ncoord = (i[3], i[4])
+                #formula para la heuristica
+                heu = (((ncoord[0] - dcoord[0]) ** (2)) + ((ncoord[1] - dcoord[1]) ** (2))) ** (0.5)
+                break
+
+        return heu
+
     # ----- Algoritmo primero a lo ancho -----
     def BFS(self, nodoInicio, nodoDestino):
 
@@ -68,7 +93,7 @@ class Grafo:
         #es el diccionario que va a guardar el nodo con su padre
         padreDic = {}
         padreDic[nodoInicio] = nodoInicio
-
+        
         while len(porExplorar) > 0: #recorremos la lista hasta que se vacie
             n = None
             for v in porExplorar:
@@ -76,8 +101,8 @@ class Grafo:
                     #o que el nodo tenga un nivel de arbol menor al nodo que explorare
                     #esto porque sigo explorando el mismo nivel, no avanzo
                     n = v;
-
-            print('nodo que voy llegando ',n)        
+       
+            print('nodo que voy llegando',n)
              #comprobar si llegamos al final
             if n == nodoDestino:
                 ruta = []
@@ -87,29 +112,20 @@ class Grafo:
                     #iteramos siempre que no se haya llegado al nodo inicial - 1
                     ruta.append(n) #pusheamos el nodo a la ruta
                     n = padreDic[n]  # n ahora toma el valor de su padre
-                    
-
+                
                 ruta.append(nodoInicio) # el iinicio lo insertamos porque no estaba 
                 #y revertimos el arreglo de la nueva ruta
                 ruta.reverse()
-                print('Ruta final: ',ruta)
                 return ruta
 
             for nodoConexion, arbolNivel in self.verticesConexion(n): #iteramos en las conexiones del nodo actual
                 #nodoConexion es la conexion del nodo n
-                #dist es la arbolNivel entre estos 2 nodos
                 #si el nodo no esta en explorados ni por explorar lo agrego
                 if nodoConexion not in porExplorar and nodoConexion not in yaExplorados:
                     porExplorar.add(nodoConexion) #se actualiza para explorar
                     padreDic[nodoConexion] = n   #{'S': 'S', 'A': 'S', 'D': 'S'} obtengo padre e hijo
                     d[nodoConexion] = d[n] + 1 # d = {'S': 0, 'A': 1, 'D': 1} asigno el nivel del "arbol", 1
-                    #d[nodoConexion] es el puro nivel                
-                else:
-
-                    if d[nodoConexion] > d[n] + 1: #si el nivel de mi conexion es mayor que mi nivel actual + 1
-                        d[nodoConexion] = d[n] + 1 #le asigno ese nuevo nivel  que lo hace bajar 1
-                        padreDic[nodoConexion] = n #n ahora es parte de los padres
-                        
+            
             if n == None:
                 return None
             
@@ -117,8 +133,7 @@ class Grafo:
             porExplorar.remove(n)
             yaExplorados.add(n)
             
-        print('La ruta no existe pai ')
-        return None
+        return ('La ruta no existe pai ')
 
     # ----- Algoritmo Primero Profundidad -----
     def DFS(self,nodoInicio, nodoDestino):
@@ -147,7 +162,7 @@ class Grafo:
                     n = v;
                     
                          
-            print('nodo que voy llegando',n)                
+           # print('nodo que voy llegando',n)                
              #comprobar si llegamos al final
             if n == nodoDestino:
                 ruta = []
@@ -161,7 +176,6 @@ class Grafo:
                 ruta.append(nodoInicio) # el iinicio lo insertamos porque no estaba 
                 #y revertimos el arreglo de la nueva ruta
                 ruta.reverse()
-                print('Ruta final: ',ruta)
                 return ruta
             
             for nodoConexion, arbolNivel in self.verticesConexion(n): #iteramos en las conexiones del nodo actual
@@ -174,7 +188,6 @@ class Grafo:
                     d[nodoConexion] = d[n] + 1 # d = {'S': 0, 'A': 1, 'D': 1} asigno el nivel del "arbol", 1
                     #d[nodoConexion] es el puro nivel                
                 else:
-
                     if d[nodoConexion] > d[n] + 1: #si el nivel de mi conexion es mayor que mi nivel actual + 1
                         d[nodoConexion] = d[n] + 1 #le asigno ese nuevo nivel  que lo hace bajar 1
                         padreDic[nodoConexion] = n #n ahora es parte de los padres
@@ -190,8 +203,7 @@ class Grafo:
             porExplorar.remove(n)
             yaExplorados.add(n)
             
-        print('La ruta no existe pai ')
-        return None
+        return ('La ruta no existe pai ')
     
     # ----- Algoritmo profundidad Limitada -----
     def DFSLim(self,nodoInicio, nodoDestino):
@@ -210,6 +222,7 @@ class Grafo:
         padreDic[nodoInicio] = nodoInicio
 
         limite = 2
+        
         while len(porExplorar) > 0: #recorremos la lista hasta que se vacie
             n = None
             
@@ -221,7 +234,8 @@ class Grafo:
                       if(d[v]==limite): #si llego al limite, entonces es el destino
                         #ya llegue porque llegue a mi limite
                         nodoDestino = n
-
+               
+            print('nodo que voy llegando ',n)  
              #comprobar si llegamos al final
             if n == nodoDestino:
                 ruta = []
@@ -236,7 +250,6 @@ class Grafo:
                 ruta.append(nodoInicio) # el iinicio lo insertamos porque no estaba 
                 #y revertimos el arreglo de la nueva ruta
                 ruta.reverse()
-                print('Ruta final: ',ruta)
                 return ruta
 
             for nodoConexion, arbolNivel in self.verticesConexion(n): #iteramos en las conexiones del nodo actual
@@ -253,9 +266,6 @@ class Grafo:
                     if d[nodoConexion] > d[n] + 1: #si el nivel de mi conexion es mayor que mi nivel actual + 1
                         d[nodoConexion] = d[n] + 1 #le asigno ese nuevo nivel  que lo hace bajar 1
                         padreDic[nodoConexion] = n #n ahora es parte de los padres
-                        if nodoConexion in yaExplorados:
-                            yaExplorados.remove(nodoConexion)
-                            porExplorar.add(nodoConexion)
 
                         
             if n == None:
@@ -265,8 +275,7 @@ class Grafo:
             porExplorar.remove(n)
             yaExplorados.add(n)
             
-        print('La ruta no existe pai ')
-        return None
+        return ('La ruta no existe pai ')
     
     # ----- Algoritmo profundidad iterativa -----
     def IDFS(self,nodoInicio, nodoDestino,limite):
@@ -285,8 +294,6 @@ class Grafo:
         padreDic[nodoInicio] = nodoInicio
 
 
-
-
         while len(porExplorar) > 0: #recorremos la lista hasta que se vacie
             n = None
 
@@ -295,13 +302,14 @@ class Grafo:
                     #Siempre y cuando el limite sea igual o menor, si llega
                     n = v;
 
+            print('nodo al que llego',n)
             if n == None: # aqui hacemos recursividad para aumentar por 1 el limte
                 #en caso que el nodo sea None saliendo de los explorados, le agregamos un nivel a 
                 #recorrer en el arbol para buscar 
                 limite += 1
                 self.IDFS(nodoInicio, nodoDestino,limite)
                 return None
-                        
+                      
              #comprobar si llegamos al final
             if n == nodoDestino:
                 ruta = []
@@ -316,7 +324,6 @@ class Grafo:
                 ruta.append(nodoInicio) # el iinicio lo insertamos porque no estaba 
                 #y revertimos el arreglo de la nueva ruta
                 ruta.reverse()
-                print('Ruta final: ',ruta)
                 return ruta
 
             for nodoConexion, arbolNivel in self.verticesConexion(n): #iteramos en las conexiones del nodo actual
@@ -329,7 +336,7 @@ class Grafo:
                     d[nodoConexion] = d[n] + 1 # d = {'S': 0, 'A': 1, 'D': 1} asigno el nivel del "arbol", 1
                     #d[nodoConexion] es el puro nivel                
                 else:
-
+                    
                     if d[nodoConexion] > d[n] + 1: #si el nivel de mi conexion es mayor que mi nivel actual + 1
                         d[nodoConexion] = d[n] + 1 #le asigno ese nuevo nivel  que lo hace bajar 1
                         padreDic[nodoConexion] = n #n ahora es parte de los padres
@@ -341,8 +348,7 @@ class Grafo:
             porExplorar.remove(n)
             yaExplorados.add(n)
             
-        print('La ruta no existe pai ')
-        return None
+        return ('La ruta no existe pai ')
             
     
     # ----- Algoritmo hill climbing -----
@@ -438,5 +444,63 @@ class Grafo:
 
     # ----- Algoritmo beam search -----
     def beam(self, nodoInicio, nodoDestino, w):
-        print("hola mundo")   
+        print("hola mundo")
+        
+    def aEstrella(self, nodoInicio, nodoDestino):
+
+        self.nodoDestino = nodoDestino
+
+        porExplorar = set([nodoInicio])
+        yaExplorados = set([])
+
+        d = {}
+        d[nodoInicio] = 0
+
+        padreDic = {}
+        padreDic[nodoInicio] = nodoInicio
+        
+        while len(porExplorar) > 0:
+            n = None
+            for v in porExplorar:
+                if n == None or d[v] + self.heuristica(v) < d[n] + self.heuristica(n):
+                    n = v;
+
+
+             #comprobar si llegamos al final
+            if n == nodoDestino:
+                ruta = []
+                while padreDic[n] != n: #buscamos la llave que contenga el valor de N 
+                    #o bien el padre encuentra a su hijo,
+                    #tipo n vale B, entonces encuentra a su hijo C y asi iterativamente
+                    #iteramos siempre que no se haya llegado al nodo inicial - 1
+                    ruta.append(n) #pusheamos el nodo a la ruta
+                    n = padreDic[n]  # n ahora toma el valor de su padre
+                    
+
+                ruta.append(nodoInicio) # el iinicio lo insertamos porque no estaba 
+                #y revertimos el arreglo de la nueva ruta
+                ruta.reverse()
+                return ruta
+
+            for (nodoConexion, peso) in self.verticesConexion(n):
+
+                if nodoConexion not in porExplorar and nodoConexion not in yaExplorados:
+                    porExplorar.add(nodoConexion)
+                    padreDic[nodoConexion] = n
+                    d[nodoConexion] = d[n] + peso
+
+                else:
+                    if d[nodoConexion] > d[n] + peso:
+                        d[nodoConexion] = d[n] + peso
+                        padreDic[nodoConexion] = n
+
+                        if nodoConexion in yaExplorados:
+                            yaExplorados.remove(nodoConexion)
+                            porExplorar.add(nodoConexion)
+
+            porExplorar.remove(n)
+            yaExplorados.add(n)
+        
+        return ('No se encontro un camino')
+    
         
